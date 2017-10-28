dffba8e48fe8351b1fa8d03ca9aa6741d31d05e2
sebe2017 <secheverria@moldeable.com>
2017-10-27 21:45:26 -0300

    cambiada sqlite por postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171026231135) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pets", force: :cascade do |t|
     t.string "description"
     t.string "address"
