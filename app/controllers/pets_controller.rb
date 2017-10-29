6e8c16dddbe32818f39c7db40fe9740d7fea0397
sebe2017 <secheverria@moldeable.com>
2017-10-29 00:51:07 -0300

    Activada Google map api y autolocalización :D

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -25,10 +25,8 @@ class PetsController < ApplicationController
   # POST /pets.json
   def create
     @pet = Pet.new(pet_params)
-
     respond_to do |format|
       if @pet.save
-        @pet.set_coordinates(request) if @pet.address.empty?
         format.html { redirect_to @pet, notice: 'Pet was successfully created.' }
         format.json { render :show, status: :created, location: @pet }
       else
