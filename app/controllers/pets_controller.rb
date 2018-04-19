a65cc93ac8d97ecfb24403b8500148ba74a48d80
Victoriosu <seb.echeverria@alumnos.duoc.cl>
2018-04-19 20:21:18 -0300

    Sin verify para las mascotas (restaurar luego)

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,6 @@
 class PetsController < ApplicationController
   before_action :set_pet, only: [:show, :edit, :update, :destroy]
-
+  skip_before_filter :verify_authenticity_token
   # GET /pets
   # GET /pets.json
   def index
